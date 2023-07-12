f4ad690dbc416fbbd145b1301d8c1dbec316eba5
Ilya <ilyadronov2332@gmail.com>
2023-07-12 23:25:56 +0300

    feat: added redis service to to the application

--- app/main.py
+++ app/main.py
@@ -5,12 +5,14 @@ from sqlalchemy.sql import text
 from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
+from redis import asyncio as aioredis
 
 from .db.database import get_db
 from .config import settings
 
 
 app = FastAPI()
+redis = aioredis.Redis(host="redis", port=6379, db=0)
 
 
 origins = [
@@ -38,6 +40,15 @@ async def test_postgres(db: AsyncSession = Depends(get_db)):
     except SQLAlchemyError as e:
         return {"message": "Database connection failed", "exception": f"{e.args[0]}"}
     
+
+@app.get("/test-redis")
+async def test_redis():
+    await redis.set("Ryan Gosling", "Literally me")
+    value = await redis.get("Ryan Gosling")
+    if value:
+        return {"message": f"Redis works! Ryan gosling is {value.decode()}"}
+    return {"error": "You are not Ryan Gosling, redis doesn't work"} 
+
 
 
 if __name__ == '__main__':
